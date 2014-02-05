d97a78c8ad18258d2e14478838fbb36ce1beb436
chethan <che4on@gmail.com>
2014-02-05 10:09:50 +0530

    added custom.js , removed tweet posting function in app.js

--- app.js
+++ app.js
@@ -94,11 +94,7 @@ app.get('/auth/twitter/callback', function(req, res, next){
                         .verifyCredentials(function (err, data) {
                             console.log(err,data);
                         })
-                        .updateStatus('Test tweet from ntwitter/' + twitter.VERSION,
-                        function (err, data) {
-                            console.log(err,data);
-                            res.redirect('/');
-                        }
+                        
                     );
 
                 }
